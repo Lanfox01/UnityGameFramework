f247621095a325f2407b04ebe3c5ac2759a8493e
Lanfox01 <lyming.08@163.com>
2024-06-11 08:47:05 +0800

    gs11

--- Scripts/Runtime/Base/BaseComponent.cs
+++ Scripts/Runtime/Base/BaseComponent.cs
@@ -232,6 +232,7 @@ namespace UnityGameFramework.Runtime
             GameFrameworkEntry.Update(Time.deltaTime, Time.unscaledDeltaTime);
             //驱动所有模块的 update
             //GameFramework.Event.EventManager
+            Debug.Log("EditorResourceMode "+ EditorResourceMode);
         }
 
         private void OnApplicationQuit()
